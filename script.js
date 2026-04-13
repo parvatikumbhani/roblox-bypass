f0c65dac9f18b971013ff83198ed2fffbc279289
aahan2000a <83102405+parvatikumbhani@users.noreply.github.com>
2026-04-12 21:04:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function loadPage(url) {
-    document.getElementById("rbxFrame").src = url;
+async function loadInfo() {
+    const res = await fetch("/api/search");
+    const data = await res.json();
+
+    document.getElementById("name").innerText = data.name;
+    document.getElementById("players").innerText = "Players Online: " + data.playerCount;
+    document.getElementById("icon").src = data.icon;
 }
+
+loadInfo();
